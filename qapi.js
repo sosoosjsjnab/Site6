96ea2cf7c28c5efc495364334eadf249b0c553ed
sosoosjsjnab <74925201+sosoosjsjnab@users.noreply.github.com>
2020-12-10 00:13:57 +0300

    Initial commit

---
+++ qapi.js
@@ -0,0 +1,59 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"5fd1396104e716001efc6202",
+				"5fd1396104e716001efc6204",
+				"5fd13a16e563f3001ecb8bd5",
+				"5fd13c78d84ef0001e948fa7",
+				"5fd13c9e04e716001efc6216"
+			]
+		},
+		"5fd1396104e716001efc6202": {
+			"id": "5fd1396104e716001efc6202",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"5fd1396104e716001efc6204": {
+			"id": "5fd1396104e716001efc6204",
+			"name": "index",
+			"pageUrl": "index",
+			"seo": {
+				"title": "Winston Churchill - biography",
+				"description": "Winston Churchill - biography",
+				"og:description": "Winston Churchill - biography",
+				"og:title": "Winston Churchill - biography",
+				"favicon_32px": "https://uploads.quarkly.io/5fd1396104e716001efc61ff/images/5b7efc81370f2c300f8b4605.jpg?v=2020-12-09T21:10:26.144Z"
+			}
+		},
+		"5fd13a16e563f3001ecb8bd5": {
+			"id": "5fd13a16e563f3001ecb8bd5",
+			"pageUrl": "terms",
+			"name": "Terms",
+			"seo": {
+				"title": "Terms And Conditions"
+			}
+		},
+		"5fd13c78d84ef0001e948fa7": {
+			"id": "5fd13c78d84ef0001e948fa7",
+			"pageUrl": "privacy",
+			"name": "Privacy",
+			"seo": {
+				"title": "Privacy Policy"
+			}
+		},
+		"5fd13c9e04e716001efc6216": {
+			"id": "5fd13c9e04e716001efc6216",
+			"pageUrl": "cookies",
+			"name": "Cookies",
+			"seo": {
+				"title": "Cookies"
+			}
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby"
+}
---
+++ src/pages/index.js
@@ -0,0 +1,131 @@
+import React from "react";
+import theme from "theme";
+import { Theme, Link, Text, Icon, Box, Image } from "@quarkly/widgets";
+import { Helmet } from "react-helmet";
+import { Override, StackItem, Stack, Section } from "@quarkly/components";
+import { MdArrowDownward } from "react-icons/md";
+export default (() => {
+	return <Theme theme={theme}>
+		<Helmet>
+			<title>
+				Winston Churchill - biography
+			</title>
+			<meta name={"description"} content={"Winston Churchill - biography"} />
+			<meta property={"og:title"} content={"Winston Churchill - biography"} />
+			<meta property={"og:description"} content={"Winston Churchill - biography"} />
+			<link rel={"shortcut icon"} href={"https://uploads.quarkly.io/5fd1396104e716001efc61ff/images/5b7efc81370f2c300f8b4605.jpg?v=2020-12-09T21:10:26.144Z"} type={"image/x-icon"} />
+		</Helmet>
+		<Section
+			background="linear-gradient(0deg,rgba(4, 8, 12, 0.6) 0%,rgba(4, 8, 12, 0.6) 100%),--color-darkL2 url(https://uploads.quarkly.io/5fd1396104e716001efc61ff/images/5b7efc81370f2c300f8b4605.jpg?v=2020-12-09T21:10:26.144Z) center/cover"
+			padding="64px 0"
+			sm-padding="40px 0"
+			color="--light"
+			font="--base"
+		>
+			<Stack>
+				<StackItem width="75%" lg-width="100%">
+					<Override slot="StackItemContent" flex-direction="column" />
+					<Text as="h1" font="--headline1" md-font="--headline2" margin="10px 0">
+						Winston Churchill - biography
+					</Text>
+				</StackItem>
+			</Stack>
+			<Box text-align="center" margin="96px 0 0 0">
+				<Icon category="md" margin="0 auto" icon={MdArrowDownward} />
+			</Box>
+		</Section>
+		<Section padding="100px 0" sm-padding="40px 0">
+			<Override slot="SectionContent" align-items="center" />
+			<Text as="p" font="--lead" margin="20px 0 0 0">
+				On November 30, 1874, in Blenheim, the family estate of the British dukes of Marlborough, Randolph Churchill and his wife Janie, the daughter of the American millionaire Leonard Jerome, had a son, who was named Winston. Neither Winston himself nor his father - the third son of the seventh Duke of Marlborough, however, did not inherit the ducal title, since it is passed on to the eldest son in England. Nevertheless, Winston (full name Winston Leonard Spencer-Churchill) belonged to the highest aristocratic family of Great Britain, and his parents tried to give him an appropriate education that would allow him to live as befits people of their circle. However, from the very beginning, with the education of young Winston (Winnie) Churchill, things went extremely unevenly. At the age of 8, the boy, according to the English aristocratic tradition, was sent to the preparatory school of St. George, but did not get along there because of his extremely wayward and self-confident character, which led to abandonment of studies and constant violations of discipline on his part. After a while, the parents, at the initiative of the boy's nanny Elizabeth Ann Everest, who, like the children of many English aristocrats who traditionally pay little attention to family affairs, actually replaced his parents, Winston was transferred to Brighton, to the Thomson sisters' school. There Winston began to study a little better, but was the worst student in his class. Many teachers considered him limited and not very capable of science, although in reality he was just extremely stubborn and wayward, so he conscientiously taught only what was interesting to him (for example, English and literature). Winston Churchill himself, already being what he eventually became, admitted that he was an extremely poor student, and only the teacher's rods, widely practiced at that time in British schools, allowed him to master some elementary academic disciplines. At the same time, he sometimes amazed teachers by knowing by heart long passages and scenes from the works of Shakespeare. However, professional school psychology was not yet developed at that time, and the opinion prevailed in pedagogical circles that the main reason for school failure could only be the naturally low giftedness of the student. No one could have imagined that once this chronically lagging behind in most school subjects, characterized by intolerable behavior and at the same time very poor health, the child would become one of the most brilliant personalities in world history, one of those who would save Europe and Asia from the violence of countries -agressors.
+			</Text>
+		</Section>
+		<Section text-align="center" padding="100px 0" sm-padding="40px 0">
+			<Text as="h1" font="--headline1" md-font="--headline2" margin="20px 0 0 0">
+				By the time Winston finished his general education{"\n\n"}
+			</Text>
+			<Text as="p" font="--lead" margin="20px 0 0 0">
+				His father had already ceased to harbor hopes that he would become a highly educated, as befits him, an aristocrat and engage in legal activity. From 1886 he studied at the famous public school for boys Harrow, where he suddenly showed a great interest in the military disciplines, in addition, became the champion of the school in fencing. Therefore, the family decided that the matured Winston should continue his education at a military school. In 1893, with great difficulty and a low overall score (the need to pass an exam in Latin failed) Winston Churchill entered the Royal Military School in Sandhurst, but there, unexpectedly for his relatives accustomed to his negligence, he showed high talent and diligence, quickly became one of the best students and after eighteen months graduated from college with honors. In 1895 he received the rank of junior lieutenant and was enlisted in the 4th Royal Hussar Regiment. In the same year, his father and beloved nanny died. During this period, Great Britain waged active colonial wars: before, it seemed unshakable, it was crowded in the world market by the United States and Germany, whose economies were based on free enterprise. The conservative aristocratic elite of the British Empire, which traditionally saw the well-being of their power in the exploitation of the colonies, tried to regain their lost positions in the world economy through new colonial conquests. The British army then occupied new lands in economically less developed countries, then suppressed the uprisings provoked by the intensification of exploitation in the old colonies. This allowed British officers to pursue a high career in the military. In the late 90s. XIX century. Churchill took part in military expeditions to India and Sudan. Now, when the teacher's stick was not hanging over him, he is calmly engaged in self-education. But his main passion in those years was literary activity. With several British newspapers, he began to collaborate as a war correspondent. Subsequently, during his life, he published a number of essays, which are still of interest to both ordinary readers and specialists, on historical topics: "The World Crisis", "World War II", etc. He also wrote several fiction novels. Churchill liked both the literary work itself and the large royalties he received from the publication of his works. Winston Churchill had all the chances one day to take the highest military post and become a famous writer. Gradually, however, he, possessing an excellent analytical mind and indomitable energy, began to be attracted to political activity. Then, as always, obeying his violent ambitions, he left the army, where he was constrained by his official duties, and in June 1899 he established contact with the Central Bureau of the Conservative Party. The Conservative Party, which advocated the traditional path of British government development based on aristocracy and great power, and the liberal party, which advocated the expansion of free enterprise and civil liberties, were then the main political forces in the domestic political arena of the state. In those years, representatives of the Conservative Party also held senior government positions in Great Britain. As a representative of the highest English aristocracy, Churchill was mainly sympathetic to the Conservatives, but, being able to think soberly and without bias, he recognized some of the merits of the liberal program. From the very first steps of his political activity, in which he joined without any problems, using the authority of his late father - also a member of the Conservative Party for some time, Churchill combined assertiveness, straightforwardness and eloquence with sly, intriguing tactics. In 1900, he was first elected to the British House of Commons as a Conservative MP. Immediately, he began attempts to gain the greatest authority for himself in the party, in order to quickly be promoted to its leading positions and then get a place in the government. For this, he, delivering convincing and incendiary speeches at party meetings, began to criticize the activities of the party leadership, gathering around him a certain group of supporters. He hoped that the party leaders, fearing a split, would include him in the government to demonstrate their loyalty to the inner-party wing he had formed.
+			</Text>
+			<Box display="flex" margin="40px 0 20px 0" justify-content="space-around" sm-flex-direction="column">
+				<Box padding="10px">
+					<Image src="https://uploads.quarkly.io/5fd1396104e716001efc61ff/images/lech333ima_Stranitsa_045_Izobrazhenie_0001.jpg?v=2020-12-09T21:10:26.144Z" width="320px" max-width="100%" />
+				</Box>
+				<Box padding="10px">
+					<Image src="https://uploads.quarkly.io/5fd1396104e716001efc61ff/images/5zJNVMRRrEXOqIz2TvkhO2HIyJs.jpg?v=2020-12-09T21:10:26.137Z" width="320px" max-width="100%" />
+				</Box>
+				<Box padding="10px">
+					<Image src="https://uploads.quarkly.io/5fd1396104e716001efc61ff/images/46893864585_599f98ce0d_k.jpg?v=2020-12-09T21:10:26.131Z" width="320px" max-width="100%" />
+				</Box>
+			</Box>
+		</Section>
+		<Section padding="140px 0" sm-padding="40px 0" background="url(https://uploads.quarkly.io/5fd1396104e716001efc61ff/images/a0b3450a143e57440e506a04eee264a1--uk-history-winston-churchill.jpg?v=2020-12-09T21:10:26.129Z) center/cover">
+			<Box max-width="240px" padding="50px 80px 80px 50px" background="--color-light" color="--dark">
+				<Text font="--base">
+					However, the young politician miscalculated, deciding to rush things in this way. During the formation of the government in 1903, Prime Minister Arthur Balfour refused to grant Churchill a seat. The ambitious and extremely stubborn Churchill, realizing that the Conservative Party, based on a strict hierarchy and domination of the senior aristocrats, could not make a quick political career, moved to the camp of the liberals, which caused the rejection of his former comrades-in-arms for a long time. So practically throughout his political career, adherence to principles, pride and stubbornness repeatedly threw him to the sidelines of political life, but then his sobriety and prudence returned him back to the top of fame.{"\n\n"}
+				</Text>
+			</Box>
+		</Section>
+		<Section padding="100px 0" sm-padding="40px 0">
+			<Override slot="SectionContent" align-items="center" />
+			<Text as="p" font="--lead" margin="20px 0 0 0">
+				Soon, the new world political realities, the decline of aristocracy and the conquest of domination by free enterprise in the world economy did their job: representatives of the Liberal Party, who ruled the country for 17 years, began to be elected prime ministers of Great Britain. Winston Churchill experienced a political rise again. At first, the British liberals to some extent sympathized with the German capitalists, who were able to bring their previously backward by European standards predominantly agrarian country to the advanced economic level. They did not consider it a priority to overcome the competition with the German Empire for colonial domination in the world, so they opposed an increase in arms spending. Churchill initially adhered to this point of view. However, by the middle of 1911 it became clear that the development of German capitalism was not limited to the displacement of German domestic products in the British markets. German policy combined both colonial expansionism, which is traditional for the advanced countries of Europe, and freedom of enterprise, which is more typical of the United States. The German leadership and the German imperialists did not hide their intentions to achieve world domination and oust Great Britain from her colonies, on the exploitation of which her economic well-being directly depended. It was obvious that a war for a colonial redistribution between Great Britain and Germany was inevitable, and Churchill changed his point of view, stating the need to prepare for a serious armed confrontation with a continental rival. On December 23, 1911, he was appointed Minister of the Navy. During this period, he completely switched to foreign policy issues and bringing the British navy to readiness for a major war. The Navy was the main striking force of the British armed forces due to the insular position of Great Britain, and the success of any military action it took depended on the effectiveness of its actions. Therefore, under the leadership of Churchill, a number of intensive measures were taken to improve his fighting qualities. Warships were converted from coal to fuel oil, which increased their speed; the ships were equipped with large caliber guns. Churchill assigned an important role to the selection of naval personnel. Younger, more energetic and talented officers were appointed to many leadership positions instead of the old bosses, who occupied them more on the basis of nobility than real merit, and accustomed to doing all things according to traditional tactical templates. All these measures fully justified themselves with the outbreak of the First World War and allowed Great Britain to successfully transfer its troops to the continent, preventing the formidable German fleet from reaching its shores.
+			</Text>
+		</Section>
+		<Image width="640px" height="640px" src="https://uploads.quarkly.io/5fd1396104e716001efc61ff/images/winston-churchill-45.jpg?v=2020-12-09T21:10:26.130Z" />
+		<Section padding="100px 0" sm-padding="40px 0" background="#f0f0f0">
+			<Override slot="SectionContent" align-items="center" />
+			<Text as="p" font="--lead" margin="20px 0 0 0">
+				<Link href="/terms">
+					Terms And Conditions
+				</Link>
+				{" "}-{" "}
+				<Link
+					href="/privacy"
+					overflow-wrap="normal"
+					word-break="normal"
+					white-space="normal"
+					text-indent="0"
+					text-overflow="clip"
+					hyphens="manual"
+				>
+					Privacy Policy
+				</Link>
+				{" "}-{" "}
+				<Link
+					href="/cookies"
+					overflow-wrap="normal"
+					word-break="normal"
+					white-space="normal"
+					text-indent="0"
+					text-overflow="clip"
+					hyphens="manual"
+				>
+					Cookies
+				</Link>
+			</Text>
+		</Section>
+		<Link
+			font={"--capture"}
+			font-size={"10px"}
+			position={"fixed"}
+			bottom={"12px"}
+			right={"12px"}
+			z-index={"4"}
+			border-radius={"4px"}
+			padding={"5px 12px 4px"}
+			background-color={"--dark"}
+			opacity={"0.6"}
+			hover-opacity={"1"}
+			color={"--light"}
+			cursor={"pointer"}
+			transition={"--opacityOut"}
+			quarkly-title={"Badge"}
+			text-decoration-line={"initial"}
+			href={"https://quarkly.io/"}
+			target={"_blank"}
+		>
+			Made on Quarkly
+		</Link>
+	</Theme>;
+});
